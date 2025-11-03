6919182db550d1cce9e6662324c138f75dc6f1c9
ceki19708-bit <ceki19708@gmail.com>
2025-11-03 23:34:36 +0100

    Delete pi_network_birthdayy directory

--- pi_network_birthdayy/src/integrations/supabase/client.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://zrwoatjwpvdquoeuomcy.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpyd29hdGp3cHZkcXVvZXVvbWN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE5NDIyMjgsImV4cCI6MjA3NzUxODIyOH0.AKgSoDvWUQ1ul_n7EcGKBV7qLgtO5y9xQsRQyMlQbq4'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-// Import the supabase client like this:
-// import { supabase } from "@/integrations/supabase/client";
